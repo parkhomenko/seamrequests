5b2c6ad67841ef54bf1e9346b89fbe1cbcb5ee06
parkhomenko <stanislav_parkhomenko@yahoo.com>
2013-04-17 15:56:17 +0300

    initial commit

---
+++ requests-ejb/src/main/java/com/parkhomenko/requests/User.java
@@ -0,0 +1,59 @@
+package com.parkhomenko.requests;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Entity
+@Name("user")
+@Scope(ScopeType.SESSION)
+@Table(name="users")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Long id;
+	
+	@NotNull
+	@Size(min=5, max=15)
+	private String name;
+	
+	@NotNull
+	@Size(min=5, max=15)
+	private String password;
+	
+	public User() { }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
