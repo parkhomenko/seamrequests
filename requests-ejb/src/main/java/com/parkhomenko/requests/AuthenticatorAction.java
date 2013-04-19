9af04b1a8338aa9cd16fb43e22b609c96f4771a5
parkhomenko <stanislav_parkhomenko@yahoo.com>
2013-04-19 18:49:09 +0300

    security

--- requests-ejb/src/main/java/com/parkhomenko/requests/AuthenticatorAction.java
+++ requests-ejb/src/main/java/com/parkhomenko/requests/AuthenticatorAction.java
@@ -1,40 +1,37 @@
 package com.parkhomenko.requests;
 
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
+import javax.ejb.Stateless;
 
-import org.hibernate.Session;
-import org.hibernate.criterion.Restrictions;
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
-import org.jboss.seam.security.Credentials;
-import org.jboss.seam.security.Identity;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Out;
+import org.jboss.seam.log.Log;
 
+@Stateless
+@Name("authenticator")
 public class AuthenticatorAction implements Authenticator {
-	
-	@PersistenceContext
-	private Session session;
-	
-	@In
-	private Credentials credentials;
-	
-	@In
-	private Identity identity;
 
+	@In(required=false)   
+    @Out(required=false, scope = ScopeType.SESSION)
+    private User user;
+	
+	@Logger
+	private Log log;
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
 	@Override
 	public boolean authenticate() {
-		try {
-			User user = (User)session.createCriteria(User.class)
-					.add(Restrictions.eq("name", credentials.getUsername()))
-					.add(Restrictions.eq("password", credentials.getPassword()))
-					.uniqueResult();
-			
-			if (user != null) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch (NoResultException ex) {
-			return false;
-		}
+		log.info("Authenticator", "In authenticate");
+		user.setName("Hi man");
+		return true;
 	}
 }
